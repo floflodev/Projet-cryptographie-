a2cb6b61c30d78f489b64f074641b42e8a5de9b1
floflodev <6254fxppjh@privaterelay.appleid.com>
2026-03-23 22:27:38 +0100

    Implement Caesar cipher for phrases
    Added a function to encode a phrase using the Caesar cipher with a specified shift.

--- Activité_1.py
+++ Activité_1.py
@@ -1,12 +1,15 @@
 # Activité 1
-# Objectif : Utiliser le chiffre de César
+# Objectif général : Utiliser le chiffre de César
+
+# La liste réference pour notre alphabet
+Alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # 1) 
+# Objectif : 
+# Programmer une fonction qui renvoie le caractere de l'alphabet situé k rang apres, avec un modulo 26   
+
 def chiffre_cesar_caractere(car, k) :
-    # Vérifie si le caractere se trouve dans l'alphabet 
-    if car not in Alphabet :
-            return car
-    # Trouve l'index du caractere dns l'alphabet
+    # Trouve l'index du caractere dans l'alphabet
     i = Alphabet.index(car)
     # Décale de k positions avec un modulo 26 pour correspondre a l'alphabet
     j = (i + k)%26
@@ -16,3 +19,23 @@ def chiffre_cesar_caractere(car, k) :
 # Exemple avec Z pour le caractere et 4 pour k :
 resultat = chiffre_cesar_caractere("Z", 4)
 print(resultat) # Affiche D
+
+# 2) 
+# Objectif : 
+#Programmer une fonction qui renvoie une phrase codée par un décalage de César k 
+#Si un caractere n'est pas présent dns l'alphabet, le laissez tel quel            
+def chiffre_cesar_phrase(phrase, k):
+    nouveau_texte = ""
+    for car in phrase:
+        # Verifie si le caractere est dans l'alphabet 
+        if car not in Alphabet:
+            # Laisse le caractere tel quel si il n'est pas dans l'alphabet
+            nouveau_texte += car 
+        else:
+            # Utilise la fonction chiffre_cesar_caractere si celui ci est dans l'alphabet
+            nouvelle_lettre_phrase = chiffre_cesar_caractere(car, k)
+            # Ajoute le caractere afin de former une phrase 
+            nouveau_texte += nouvelle_lettre_phrase
+    return nouveau_texte
+# Exemple avec la phrse "CAPTUREZ IDEFIX" et 3 pour k :   
+    
